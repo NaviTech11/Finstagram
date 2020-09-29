a68c96d5c5698dbd600b9263c6f6b3ccd4104e4d
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-09-29 16:25:55 -0500

    Add Nav and Button

--- src/Components/Landing/Sidebar.js
+++ src/Components/Landing/Sidebar.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Sidebar.css';
 import Profile from './Profile';
+import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
 import InstagramIcon from '@material-ui/icons/Instagram';
+import HomeIcon from '@material-ui/icons/Home';
+import ExploreIcon from '@material-ui/icons/Explore';
+import SendIcon from '@material-ui/icons/Send';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import { IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            
+            {/* Header/Title */}
             <div className="sidebar__title">
                 <InstagramIcon className="finsta__icon"/>
                 <h1>Finstagram</h1>
@@ -27,10 +36,31 @@ function Sidebar() {
             
             {/* Navbar */}
             <div className="sidebar__navbar">
-
+                <div className="navbar__option--active">
+                    <Link to="/landing" className="navbar__link">
+                        <HomeIcon fontSize="large"/>
+                    </Link>
+                </div>
+                <div className="navbar__option">
+                    <Link to="/messenger" className="navbar__link">
+                        <SendIcon fontSize="large"/>
+                    </Link>
+                </div>
+                <div className="navbar__option">
+                    <ExploreIcon fontSize="large"/>
+                </div>
+                <div className="navbar__option">
+                    <FavoriteBorderIcon fontSize="large"/>
+                </div>
             </div>
             
             {/* Button */}
+            <div className="sidebar__btn">
+                <Button>
+                    +
+                </Button> 
+            </div>
+            
         </div>
     )
 }
