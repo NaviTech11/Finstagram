3a2a8c1a185c0071e938a813792cc129b603cffc
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-09-29 15:10:05 -0500

    Add Profile styles

--- src/Components/Landing/Profile.js
+++ src/Components/Landing/Profile.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './Profile.css';
+import { Avatar } from '@material-ui/core';
 
-function Profile() {
+function Profile({avatarSrc, name, username, followers, following,posts}) {
     return (
         <div className="profile">
             
+            <Avatar 
+                className="profile__avatar"
+                src={avatarSrc}
+            />
+    
+            <div className="profile__username">
+                <h2>{name}</h2>
+                <p>{username}</p>
+            </div>
+
+            <div className="profile__stats">
+                <div className="profile__info">
+                    <h5>{followers}</h5>
+                    <p>Followers</p>
+                </div>
+                <div className="profile__info">
+                    <h5>{following}</h5>
+                    <p>Following</p>
+                </div>
+                <div className="profile__info">
+                    <h5>{posts}</h5>
+                    <p>Posts</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Components/Landing/Sidebar.js
+++ src/Components/Landing/Sidebar.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import './Sidebar.css';
-import { Avatar } from '@material-ui/core';
+import Profile from './Profile';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             
-            <div className="title">
-                <img src="https://picsvg.com/svg/CxST2hDEVM.svg" alt="finsta-logo"/>
+            <div className="sidebar__title">
+                <InstagramIcon className="finsta__icon"/>
                 <h1>Finstagram</h1>
+                
             </div>
             
             {/* Profile */}
-            <div className="profile">
-                <Avatar 
-                    className="profile__avatar"
-                    src="https://lh3.googleusercontent.com/-KAb_q-5rvfw/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmm4RXi5191mqRF0j3dqNRuHxo_dA/s48-c/photo.jpg"
-                />
+            <div className="sidebar__profile">
+               <Profile 
+                   avatarSrc="https://lh3.googleusercontent.com/-KAb_q-5rvfw/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmm4RXi5191mqRF0j3dqNRuHxo_dA/s48-c/photo.jpg"
+                   name="Ivan Hernandez"
+                   username="@ivan.react"
+                   followers="1.46K"
+                   following="533"
+                   posts="257"
+               />
             </div>
             
             {/* Navbar */}
-            <div className="navbar">
+            <div className="sidebar__navbar">
 
             </div>
             
