a903e4bec61f82c447e5258ed010189ed91f2a79
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-09-29 22:05:09 -0500

    Add Posts and styles

--- src/Components/Landing/LandingBody.js
+++ src/Components/Landing/LandingBody.js
@@ -16,9 +16,13 @@ function LandingBody() {
             
             <div className="body__post">
                 <Post 
+                    avatarSrc="https://lh3.googleusercontent.com/-20KRdi98tlg/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucnQ15iroprPcheP31422eXD80zcVA/s48-c/photo.jpg"
+                    name="John Ashburn"
                     image="https://images.unsplash.com/photo-1564865878688-9a244444042a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
                 />
                 <Post 
+                    avatarSrc="https://lh3.googleusercontent.com/-KAb_q-5rvfw/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmm4RXi5191mqRF0j3dqNRuHxo_dA/s48-c/photo.jpg"
+                    name="Ivan Hernandez"
                     image="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
                 />
             </div>
