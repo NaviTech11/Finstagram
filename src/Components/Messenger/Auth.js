c40538c32098145dd0f867606cd634b8057228f6
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-10-02 20:38:42 -0500

    Auth change

--- src/Components/Messenger/Auth.js
+++ src/Components/Messenger/Auth.js
@@ -1,29 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { FormControl, Input } from '@material-ui/core';
+import React from 'react';
 import './Messenger.css';
-import Message from './Message';
-import db from '../../firebase';
 import firebase from 'firebase';
-import FlipMove from 'react-flip-move';
-import SendIcon from '@material-ui/icons/Send';
-import { IconButton, Grid } from '@material-ui/core';
 import 'firebase/firestore';
-import 'firebase/auth';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import {auth} from '../../firebase'
 
-const auth = firebase.auth();
 
 function SignIn() {
-
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
-
   return (
     <>
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
     </>
   )
 
